8b32bcdd598fb12a291d89d9953b62655fba1844
XRain66 <2628883576@qq.com>
2025-02-22 17:01:29 +0800

    1

---
+++ src/client/java/com/xrain/2msmod1Client.java
@@ -0,0 +1,10 @@
+package com.xrain;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class 2msmod1Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
