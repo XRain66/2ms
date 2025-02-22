527f944694ac93c92b7da7b929a58f7694d0ec38
XRain66 <2628883576@qq.com>
2025-02-22 17:20:11 +0800

    5

---
+++ src/client/java/com/xrain/TwoMSModClient.java
@@ -0,0 +1,10 @@
+package com.xrain;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TwoMSModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // 客户端初始化代码
+    }
+}
